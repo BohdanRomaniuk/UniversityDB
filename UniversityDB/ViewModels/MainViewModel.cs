8911e4830f80ded9121d24a632bdccebdc8ac8d5
romanparobiy <romik1009@gmail.com>
2018-10-12 16:55:22 +0300

    Removed list for future generatons

--- UniversityDB/ViewModels/MainViewModel.cs
+++ UniversityDB/ViewModels/MainViewModel.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using UniversityDB.Infrastructure;
 using UniversityDB.Models;
-using System.Data.Entity;
 using System.Windows.Input;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,12 +13,6 @@ namespace UniversityDB.ViewModels
     public class MainViewModel : INotifyPropertyChanged
     {
         private ObservableCollection<UObject> faculties;
-
-        // holds reference to the object, on expand every element will be 
-        // added to this list and to children of the parent element. allObject is used
-        // to avoid searching object with specific id in the tree structure
-        // when doing specific operations by id
-        private List<UObject> allObjects = new List<UObject>();
 
         // used to make every tree node expandable, will be replaced with real objects
         // as soon as they will be got from the DB
@@ -66,19 +55,14 @@ namespace UniversityDB.ViewModels
                 if (root != null)
                 {
                     Faculties.Add(root);
-                    allObjects.Add(root);
                     root.Childrens = new ObservableCollection<UObject> { dummyLoadingObject };
                 }
             }
         }
 
-        // Should be moved out of here
         private void ExecuteExpandingCommand(object obj)
         {
-            var args = obj as RoutedEventArgs;
-            var treeViewItem = args.Source as TreeViewItem;
-            var data = treeViewItem.DataContext as UObject; // Here is all we need
-            var id = data.Id;
+            var data = ((obj as RoutedEventArgs).Source as TreeViewItem).DataContext as UObject;
             AppendChildrenByParent(data);
         }
 
@@ -97,10 +81,6 @@ namespace UniversityDB.ViewModels
                         {
                             parent.Childrens.Add(child);
                             child.Childrens = new ObservableCollection<UObject> { dummyLoadingObject };
-                        }
-                        if(!allObjects.Any(c => c.Id == child.Id))
-                        {
-                            allObjects.Add(child);
                         }
                     }
                 }
