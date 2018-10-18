4ed8d78dd9b755c305063169eb0a50871a87511c
Bohdan Romaniuk <bohdan2307@gmail.com>
2018-10-18 18:50:10 +0300

    Start working on other forms

--- UniversityDB/Forms/UStudyingPersonWindow.xaml.cs
+++ UniversityDB/Forms/UStudyingPersonWindow.xaml.cs
@@ -5,17 +5,17 @@ using UniversityDB.ViewModels;
 
 namespace UniversityDB.Forms
 {
-    public partial class UStudyingPerson : Window
+    public partial class UStudyingPersonWindow : Window
     {
         //View and Edit
-        public UStudyingPerson(UObject elem, FormType type)
+        public UStudyingPersonWindow(UObject elem, FormType type)
         {
             DataContext = new UObjectViewModel(elem, type);
             InitializeComponent();
         }
 
         //Adding
-        public UStudyingPerson(UObject elem, FormType type, string className)
+        public UStudyingPersonWindow(UObject elem, FormType type, string className)
         {
             DataContext = new UObjectViewModel(elem, type, className);
             InitializeComponent();
